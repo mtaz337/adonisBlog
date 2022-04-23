9c47362bbe2b1607c3093961cd48275e90ddc7e2
Mahir Tazwar <mahirtaz@gmail.com>
2022-04-24 02:21:55 +0600

    development phase 4

--- app/Controllers/Http/FriendController.js
+++ app/Controllers/Http/FriendController.js
@@ -1,5 +1,8 @@
 'use strict'
 const Friend = use('App/Models/Friend')
+
+const {validate} = use('Validator')
+
 class FriendController {
   async index({view}){
    
@@ -19,6 +22,72 @@ class FriendController {
   async add({view}){
     return view.render('addfriend')
   }
+
+  async store({request,response,session}){
+
+    const validation = await validate(request.all(),{
+      friendName: 'required|min:3|max:20',
+      photo: 'required|min:10|max:255',
+      job: 'required|min:4|max:50',
+      description: 'required|min:10|max:255',
+    })
+    if(validation.fails()){
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+    const friend = new Friend();
+
+    friend.name = request.input('friendName')
+    friend.photo = request.input('photo')
+    friend.job = request.input('job')
+    friend.descriptions = request.input('description')
+
+    await friend.save()
+    session.flash({ notification:'New Friend Added'})
+
+    return response.redirect('/friends/')
+  }
+
+  async edit({params,view}){
+    const friend = await Friend.find(params.id)
+    return view.render('editfriend',{
+      friend: friend
+    })
+   }
+
+   async update({view,params,request,response,session}){
+     //adding input validations
+    const validation = await validate(request.all(),{
+      friendName: 'required|min:3|max:20',
+      photo: 'required|min:10|max:255',
+      job: 'required|min:4|max:50',
+      description: 'required|min:10|max:255',
+    })
+    if(validation.fails()){
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+
+     const friend = await Friend.find(params.id)
+
+     friend.name = request.input('friendName')
+     friend.photo = request.input('photo')
+     friend.job = request.input('job')
+     friend.descriptions = request.input('description')
+     
+     await friend.save()
+     session.flash({notification:'friend Description Edited'})
+
+     return response.redirect('/friends')
+   }
+   async destroy({params,session,response}){
+   const friend = await friend.find(params.id)
+   
+   await friend.delete()
+
+   session.flash({notification:'A friend Was Delete from The List'})
+   return response.redirect('/friends/')
+   }
 
 }
 
--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -2,6 +2,8 @@
 
 const { post } = require("@adonisjs/framework/src/Route/Manager")
 const Fruit = use('App/Models/Fruit')
+//adding the validator
+const {validate} = use('Validator')
 class PostController {
   async index({view}){
     const fruits = await Fruit.all()
@@ -22,6 +24,18 @@ class PostController {
     return view.render('addFruit')
   }
   async store({request,response,session}){
+    //adding input validations
+    const validation = await validate(request.all(),{
+      fruitName: 'required|min:3|max:20',
+      photo: 'required|min:10|max:255',
+      taste: 'required|min:4|max:50',
+      description: 'required|min:10|max:255',
+    })
+    if(validation.fails()){
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+
     const fruit = new Fruit();
 
     fruit.name = request.input('fruitName')
@@ -34,6 +48,48 @@ class PostController {
 
     return response.redirect('/fruits/')
   }
+
+  async edit({params,view}){
+    const fruit = await Fruit.find(params.id)
+    return view.render('editFruit',{
+      fruit: fruit
+    })
+   }
+
+   async update({view,params,request,response,session}){
+     //adding input validations
+    const validation = await validate(request.all(),{
+      fruitName: 'required|min:3|max:20',
+      photo: 'required|min:10|max:255',
+      taste: 'required|min:4|max:50',
+      description: 'required|min:10|max:255',
+    })
+    if(validation.fails()){
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+
+     const fruit = await Fruit.find(params.id)
+
+     fruit.name = request.input('fruitName')
+     fruit.photo = request.input('photo')
+     fruit.tastes = request.input('taste')
+     fruit.description = request.input('description')
+     
+     await fruit.save()
+     session.flash({notification:'Fruit Description Edited'})
+
+     return response.redirect('/fruits')
+   }
+   async destroy({params,session,response}){
+   const fruit = await Fruit.find(params.id)
+   
+   await fruit.delete()
+
+   session.flash({notification:'A Fruit Was Delete from The List'})
+   return response.redirect('/fruits/')
+   }
+  
 }
 
 module.exports = PostController
